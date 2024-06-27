804bec12e867f5a36b39a7ad0ecae387c5fa1b84
Denis <ddziubek.11@gmail.com>
2024-06-28 01:42:23 +0200

    added sorting

--- pe/src/App.jsx
+++ pe/src/App.jsx
@@ -6,16 +6,41 @@ function App() {
   const [isAddEntryModalOpen, setIsAddEntryModalOpen] = useState(false);
   const [entries, setEntries] = useState([]);
 
+
+
   useEffect(() => {
     const savedEntries = JSON.parse(localStorage.getItem("diaryEntries")) || [];
     setEntries(savedEntries);
   }, []);
+
 
   const handleSave = (newEntry) => {
     const updatedEntries = [...entries, newEntry];
     setEntries(updatedEntries);
     localStorage.getItem("diaryEntries", JSON.stringify(updatedEntries));
   };
+
+  const sortAsc = () => {
+    const sortedAsc = entries.sort(function(a,b){
+      // Turn your strings into dates, and then subtract them
+      // to get a value that is either negative, positive, or zero.
+      //sorts by newest date
+      return new Date(b.date) - new Date(a.date);
+    });
+    console.log(sortedAsc);
+    setEntries(sortedAsc);
+  }
+
+  const sortDesc = () => {
+    const sortedDesc = entries.sort(function(a,b){
+      // Turn your strings into dates, and then subtract them
+      // to get a value that is either negative, positive, or zero.
+      //sorts by oldest date
+      return new Date(a.date) - new Date(b.date);
+    });
+    console.log(sortedDesc);
+    setEntries(sortedDesc);
+  }
 
   return (
     <div>
@@ -32,6 +57,12 @@ function App() {
           onClick={() => setIsAddEntryModalOpen(true)}
         >
           Add Entry
+        </button>
+        <button className='bg-white' onClick={() => sortAsc()}>
+          sortAsc
+        </button>
+        <button className='bg-white' onClick={() => sortDesc()}>
+          sortDesc
         </button>
       </header>
       <main >

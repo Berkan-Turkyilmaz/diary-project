5be96766931f07b74c50e5817cb9205917b480aa
Denis <ddziubek.11@gmail.com>
2024-06-28 01:02:57 +0200

    fixes

--- pe/src/Components/EntryModal.jsx
+++ pe/src/Components/EntryModal.jsx
@@ -4,20 +4,22 @@ const EntryModal = ({ entry, onClose }) => {
   if (!entry) return null;
 
   return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center">
-      <div className="bg-white p-6 rounded shadow-lg h-screen w-screen max-w-md max-h-md">
-        <div>
-          <h2 className="text-xl font-bold mb-4 text-center">{entry.title}</h2>
-          <p className="text-m font-semibold mb-4">Date: {entry.date}</p>
-        </div>
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex  items-center justify-center">
+      <div className="bg-white p-6 rounded shadow-lg h-screen w-screen max-w-md max-h-md overflow-auto">
         
-        <div className=" w-screen mb-4">
-          <img  src={entry.imageUrl} alt="Full entry" />
-        </div>
-        <p className='w-full p-2 border rounded'>{entry.content}</p>
+          <h2 className="text-xl font-bold mb-4 text-center">{entry.title}</h2>
+          <p className="text-m font-semibold mb-4 text-gray-500">Date: {entry.date}</p>
+        
+        
+        
+          <img  className=" w-[100%] h-60 object-cover rounded mb-4"
+          src={entry.imageUrl} alt="Full entry" />
+        
+        <p className='w-[100%] h-fit p-2 border rounded text-pretty mb-6 shrink whitespace-normal break-words'>
+          {entry.content}</p>
         <button
           onClick={onClose}
-          className="px-4 py-2 bg-gray-300 rounded mr-2"
+          className="px-4 py-2 bg-gray-300 rounded mr-2 w-[100%]"
         >
           Close
         </button>

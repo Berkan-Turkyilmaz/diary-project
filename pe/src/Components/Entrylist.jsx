06c28d4ce78c2f614a98e803367961d851c2560b
AmarbayarBold <abnabayar@gmail.com>
2024-06-27 21:57:04 +0200

    form modified with tailwind,plus list modified but further change needed

--- pe/src/Components/Entrylist.jsx
+++ pe/src/Components/Entrylist.jsx
@@ -4,13 +4,23 @@ const EntryList = ({ entries }) => {
   const [selectedEntry, setSelectedEntry] = useState(null);
 
   return (
-    <div className="">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:gird-cols-3 gap-4">
       {entries.map((entry, index) => (
-        <div key={index} className="" onClick={() => setSelectedEntry(entry)}>
-          <h2>{entry.title}</h2>
-          <p>Date: {entry.date}</p>
-          <img src={entry.imageUrl} alt="Preview" />
-          <button>View Details</button>
+        <div
+          key={index}
+          onClick={() => setSelectedEntry(entry)}
+          className="p-4 border rounded shadow hover:shadow-lg cursor-pointer"
+        >
+          <h2 className="text-xl font-bold mb-2">{entry.title}</h2>
+          <p className="text-gray-500">Date: {entry.date}</p>
+          <img
+            className="w-[60%] h-40 object-cover rounded"
+            src={entry.imageUrl}
+            alt="Preview"
+          />
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+            View Details
+          </button>
         </div>
       ))}
       {selectedEntry && (
